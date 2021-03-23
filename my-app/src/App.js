ea1c63356b5f8e3ad7849be3e6083bb7e9aefc76
maksim-mrug <maks.mrug.ru@gmail.com>
2021-03-23 19:04:43 +0300

    first-commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Accord from './Components/Accord';
+
+
+
+function App() {
+  return (
+    
+    <Accord />
+    
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/data.js
@@ -0,0 +1,35 @@
+export const Data = [
+    {
+        id: 1,
+        title: 'How many team members can I invite?',
+        imgSrc: 'images/icon-arrow-down.svg',
+        content: 'Tenetur sit pariatur consectetur perspiciatis amet enim deleniti vero asperiores eum architecto, temporibus vitae!',
+    },
+    {
+        id: 2,
+        title: 'What is the maximum file upload size?',
+        imgSrc: 'images/icon-arrow-down.svg',
+        content: 'Lorem,  temporibus vitae! Tenetur voluptatem deserunt fuga repellat nihil porro corrupti?',
+    },
+    {
+        id: 3,
+        title: 'How do i reset my password?',
+        imgSrc: 'images/icon-arrow-down.svg',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatem deserunt fuga repellat nihil porro corrupti?',
+
+    },
+    {
+        id: 4,
+        title: 'Can i cancel my subscription?',
+        imgSrc: 'images/icon-arrow-down.svg',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus vitae! Tenetur voluptatem deserunt fuga repellat nihil porro corrupti?',
+
+    },
+    {
+        id: 5,
+        title: 'Do you provide additional support?',
+        imgSrc: 'images/icon-arrow-down.svg',
+        content: 'Temporibus vitae! Tenetur voluptatem deserunt fuga repellat nihil porro corrupti?',
+
+    },
+]
